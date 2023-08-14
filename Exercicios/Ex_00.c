6ea8d275c6f41042e2a81c36f6ddee0ae8600a0e
Neander Fontes <neanderffontes@gmail.com>
2023-08-14 09:44:25 +0100

    ok
    ok

---
+++ Exercicios/Ex_00.c
@@ -0,0 +1,17 @@
+
+//Bibliotecas em geral:
+//Entrada e saída de dados:
+#include <stdio.h>
+
+//Função main:
+int main() {
+    //Declaração de variáveis e resultados:
+    printf("\n=================== COMEÇO DO PROGRAMA EM C ===================\n");
+    
+    //Atribuição e saída de valores das variáveis:
+    printf();
+    scanf();
+    printf("\n================================================================\n");
+
+    return 0;
+}
---
+++ Exercicios/Ex_03.c
@@ -0,0 +1,19 @@
+//  Cria um programa que leia dois números reais e que escreva o seu
+// produto. Deverá aparecer uma mensagem no ecrã a indicar ao
+// utilizador que este deve inserir o primeiro e o segundo valor.
+
+//Bibliotecas em geral:
+//Entrada e saída de dados:
+#include <stdio.h>
+
+//Função main:
+int main() {
+    //Declaração de variáveis e resultados:
+    printf("\n=================== COMEÇO DO PROGRAMA EM C ===================\n");
+    float num1, num2, numProduto;
+    //Atribuição e saída de valores das variáveis:
+    printf();
+    printf("\n================================================================\n");
+
+    return 0;
+}
