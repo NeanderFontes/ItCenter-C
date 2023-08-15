45fc69d88cf749bb496137780c9417898e6e7867
Neander Fontes <neanderffontes@gmail.com>
2023-08-15 09:27:54 +0100

    Update Ex_03
    Exercicio prático

--- Exercicios/Ex_03.c
+++ Exercicios/Ex_03.c
@@ -1,5 +1,5 @@
 //  Cria um programa que leia dois números reais e que escreva o seu
-// produto. Deverá aparecer uma mensagem no ecrã a indicar ao
+// produto. Deverá aparecer uma mensagem na tela a indicar ao
 // utilizador que este deve inserir o primeiro e o segundo valor.
 
 //Bibliotecas em geral:
@@ -15,9 +15,16 @@ int main() {
     
     //Declaração de variáveis e resultados:
     printf("\n=================== COMEÇO DO PROGRAMA EM C ===================\n");
-    float num1, num2, numProduto;
+    float num1, num2;
     //Atribuição e saída de valores das variáveis:
-    printf();
+    printf("Insira 2 valores decimais:");
+    printf("\nInsira 1º valor: ");
+    scanf("%f", &num1);
+    printf("Insira 2º valor: ");
+    scanf("%f", &num2);
+
+    //Saída de dados:
+    printf("(%.1f)x(%.1f) = %.1f", num1, num2, (num1 * num2));
     printf("\n================================================================\n");
 
     return 0;
