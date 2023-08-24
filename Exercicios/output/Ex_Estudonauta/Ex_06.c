e734e884aeae526aac6ac8f080560a3da9baf2d0
Neander Fontes <neanderffontes@gmail.com>
2023-08-24 08:44:11 +0100

    Update Ex_06.c

--- Exercicios/output/Ex_Estudonauta/Ex_06.c
+++ Exercicios/output/Ex_Estudonauta/Ex_06.c
@@ -11,14 +11,14 @@ void main() {
     
     //Declaração de variaveis e resultados:
     printf("\n=================== COMEÇO DO PROGRAMA EM C ===================\n");
-    int num, numAntecessor, numSucessor;
+    int num;
 
     //Atribuição e saída de valores das variaveis:
     printf("Digite um numero inteiro: ");
     scanf("%d", &num);
 
     //Entrada e Saida de dados formatados:
-    printf("O Antecessor de (%d) = (%d)", num, (numAntecessor = num - 1));
-    printf("\nO Sucessor de %d = (%d)", num, (numSucessor = num + 1));
+    printf("O Antecessor de (%d) = (%d)", num, (num - 1));
+    printf("\nO Sucessor de %d = (%d)", num, (num + 1));
     printf("\n================================================================\n");
 }
