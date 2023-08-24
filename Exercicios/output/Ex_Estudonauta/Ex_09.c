3f2d16e9b9e9160f4d5b9a1316139af137605711
Neander Fontes <neanderffontes@gmail.com>
2023-08-24 09:09:11 +0100

    Update Ex_09.c

--- Exercicios/output/Ex_Estudonauta/Ex_09.c
+++ Exercicios/output/Ex_Estudonauta/Ex_09.c
@@ -16,13 +16,16 @@ int main() {
 
     //Entrada e saida de dados formatados:
     printf("Nome do aluno: ");
+    fflush(stdin);
     gets(nomeAluno);
     printf("\nNota 1: ");
+    fflush(stdin);
     scanf(" %f", &nota1);
     printf("Nota 2: ");
+    fflush(stdin);
     scanf(" %f", &nota2);
 
-    printf("O alunota %s com notas: %.1f + %.1f M�dia = %.1f", nomeAluno, nota1, nota2, (media = (nota1+nota2)/2));
+    printf("O alunota %s com notas: %.1f + %.1f \nMédia = %.1f", nomeAluno, nota1, nota2, (media = (nota1 + nota2) / 2));
     //Atribui��o e sa�da de valores das vari�veis:
     printf("\n================================================================\n");
     return 0;
