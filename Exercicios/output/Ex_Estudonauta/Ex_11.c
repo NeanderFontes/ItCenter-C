d2b0f552adef13db3a442daa7cc76b4fe3e1b69b
Neander Fontes <neanderffontes@gmail.com>
2023-08-24 09:31:30 +0100

    Create Ex_11.c
    Criando exercício 11 plataforma Estudonauta.

---
+++ Exercicios/output/Ex_Estudonauta/Ex_11.c
@@ -0,0 +1,30 @@
+//Numero par ou impar utilizando operador ternário:
+
+//Bibliotecas necessárias:
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+//Função main:
+int main() {
+    //Inicialização da biblioteca ortográfica:
+    setlocale(LC_ALL, "Portuguese");
+    
+    //Inicio do programa:
+    printf("\n=================== COMEÇO DO PROGRAMA EM C ===================\n");
+    
+    //Declaração de variáveis e resultados:
+    int numEntrada;
+    char situacaoNum;
+
+    //Entrada e saída de dados formatados:
+    printf("Digite um numero inteiro: ");
+    fflush(stdin);
+    scanf("%d", &numEntrada);
+    printf("O Número %d digitado é ", numEntrada);
+    printf((numEntrada % 2 == 0) ? "Par" : "Impar");
+
+    //Final do Programa.
+    printf("\n================================================================\n");
+    return 0;
+}
