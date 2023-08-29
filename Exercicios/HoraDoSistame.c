ee37a8ae685e973698c8f6d2dd1bf2ed1b4d67f8
Neander Fontes <neanderffontes@gmail.com>
2023-08-29 10:07:44 +0100

    Update
    Atualizando Hora do sistema com dia da sema, mês nomeado através de array.

--- Exercicios/HoraDoSistame.c
+++ Exercicios/HoraDoSistame.c
@@ -27,12 +27,21 @@ int main() {
     int diaAtual = dataLocal->tm_mday;
     int mesAtual = dataLocal->tm_mon + 1;
     int anoAtual = dataLocal->tm_year + 1900;
+    char *nomeSemana[] = {"Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado"};
+    char *nomesMeses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    };
+    int diaSemana = dataLocal->tm_wday;
 
     //Entrada e saída de dados formatados:
-    printf("O Dia de hoje através do seu Sistema Operacional = %d", diaAtual);
-    printf("\nO Mês de hoje através do seu Sistema Operacional = %d", mesAtual);
-    printf("\nO Ano de hoje através do seu Sistema Operacional = %d", anoAtual);
-    printf("\nEstamos atualmente em %02d/%02d/%d", diaAtual, mesAtual, anoAtual);
+    printf("\n» Dia da semana = %s", nomeSemana[diaSemana]);
+    printf("\n» Mês atual nomeado = %s", nomesMeses[mesAtual - 1]);
+    printf("\n\n» O Data atual através do seu Sistema Operacional = %02d", diaAtual);
+    printf("\n» O Mês atual em Nº através do seu Sistema Operacional = %02d", mesAtual);
+    printf("\n» O Ano atual através do seu Sistema Operacional = %d", anoAtual);
+    printf("\n» Estamos atualmente em %02d/%02d/%d", diaAtual, mesAtual, anoAtual);
+    // Exibe o tempo atual em formato de texto
+    printf("\n» Tempo atual: %s", ctime(&tempoSistema));
     //Final do Programa.
     printf("\n================================================================\n");
     return 0;
