2d7e0ec811c3d88891d49c4807cb1b13c20794d0
Neander Fontes <neanderffontes@gmail.com>
2023-09-08 17:34:41 +0100

    Create Pl_02.c
    Criando Projeto de "Jogo da Velha", ou Jogo do Galo(Portugal).

---
+++ Exercicios/Pratica_laboral02/Pl_02.c
@@ -0,0 +1,8 @@
+// Criar um programa de Jogo do Galo ou Jogo da Velha(Brasil).
+// Objetivos: Resolver os problemas utilizando Vetores, funções e procedimentos.
+// 1) Criar procedimento para ler o nome dos jogadores
+// 2) Inicializar o vetor bidimensional
+// 3) Criar procedimeno para imprimir o tabuleiro
+// 4) Criar procedimento para o jogo:
+//     a. Indicar o jogador que está ganhando.
+//     b. Iserir coordenadas válidas para preencher tabuleiro e mostrar o mesmo.
